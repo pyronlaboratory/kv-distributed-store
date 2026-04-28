77adc4ea3590429404b4a34430d5d316f2067177
Ronit Mishra <12792626+pyronlaboratory@users.noreply.github.com>
2026-04-28 13:11:56 +0530

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,23 +1,37 @@
 import socket  # noqa: F401
+import selectors
+
+sel = selectors.DefaultSelector()
+
+
+def accept(sock, mask):
+    conn, addr = sock.accept()
+    print("accepted", conn, "from", addr)
+    conn.setblocking(False)
+    sel.register(conn, selectors.EVENT_READ, read)
+
+
+def read(conn, mask):
+    data = conn.recv(1024)
+    if data:
+        if b"*1\r\n$4\r\nPING\r\n" in data:
+            conn.send(b"+PONG\r\n")
+    else:
+        print("closing", conn)
+        sel.unregister(conn)
+        conn.close()
 
 
 def main():
-    # print("Logs from your program will appear here!")
-
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
+    server_socket.setblocking(False)
+    sel.register(server_socket, selectors.EVENT_READ, accept)
 
     while True:
-        sock, _ = server_socket.accept()
-        while True:
-            data = sock.recv(1024)
-            if not data:
-                break
-
-            if b"*1\r\n$4\r\nPING\r\n" in data:
-                response = b"+PONG\r\n"
-                sock.sendall(response)
-
-        sock.close()
+        events = sel.select()
+        for key, mask in events:
+            callback = key.data
+            callback(key.fileobj, mask)
 
 
 if __name__ == "__main__":
