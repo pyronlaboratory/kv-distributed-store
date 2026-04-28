d67181ac430f511d4afb60d1cf496ead94f1c32c
Ronit Mishra <12792626+pyronlaboratory@users.noreply.github.com>
2026-04-28 12:53:28 +0530

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -8,7 +8,7 @@ def main():
     while True:
         sock, _ = server_socket.accept()
         data = sock.recv(1024).strip()
-        if data.startswith(b"*1\r\n$4\r\nPING\r\n"):
+        if data == "*1\r\n$4\r\nPING\r\n":
             response = b"+PONG\r\n"
             sock.sendall(response)
         sock.close()
