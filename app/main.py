f1e00ca86a43d0bd78180f9de0ad65af707d51f7
Ronit Mishra <12792626+pyronlaboratory@users.noreply.github.com>
2026-04-28 12:45:31 +0530

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -5,7 +5,12 @@ def main():
     print("Logs from your program will appear here!")
 
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    server_socket.accept()  # wait for client
+    while True:
+        sock, addr = server_socket.accept()
+        data = sock.recv(1024).strip()
+        print(f"Received data from client: {data}")
+        sock.sendall(b"+PONG\r\n")
+        sock.close()
 
 
 if __name__ == "__main__":
