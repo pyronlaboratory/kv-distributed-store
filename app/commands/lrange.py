93a8e7e63662c17c60c0d9375e608eb31ae86c7b
Ronit Mishra <12792626+pyronlaboratory@users.noreply.github.com>
2026-04-28 15:19:08 +0530

    codecrafters submit [skip ci]

--- app/commands/lrange.py
+++ app/commands/lrange.py
@@ -5,4 +5,5 @@ from app.protocols.encoder import resp_array_encoder
 def execute(args):
     key, start, stop = args[1], int(args[2]), int(args[3])
     lst, _ = store.get(key, ([], None))
-    return resp_array_encoder(lst[start : stop + 1])  # stop is inclusive
+    stop_idx = stop + 1 or None  # when stop == -1, stop+1 == 0 which is falsy → None
+    return resp_array_encoder(lst[start:stop_idx])
