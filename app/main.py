18f258f9a5a2e0ea28fc28f1a061bf964f4174fb
Ronit Mishra <12792626+pyronlaboratory@users.noreply.github.com>
2026-04-28 12:52:17 +0530

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -2,14 +2,15 @@ import socket  # noqa: F401
 
 
 def main():
-    print("Logs from your program will appear here!")
+    # print("Logs from your program will appear here!")
 
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
     while True:
-        sock, addr = server_socket.accept()
+        sock, _ = server_socket.accept()
         data = sock.recv(1024).strip()
-        print(f"Received data from client: {data}")
-        sock.sendall(b"+PONG\r\n")
+        if data.startswith(b"*1\r\n$4\r\nPING\r\n"):
+            response = b"+PONG\r\n"
+            sock.sendall(response)
         sock.close()
 
 
