2f13f3fa72762bdf2deb123ea64fe0d9a899f28f
Ronit Mishra <12792626+pyronlaboratory@users.noreply.github.com>
2026-04-28 15:31:49 +0530

    codecrafters submit [skip ci]

--- app/commands/lpop.py
+++ app/commands/lpop.py
@@ -1,7 +1,11 @@
 from app.store import store
-from app.protocols.encoder import resp_encoder
+from app.protocols.encoder import resp_encoder, resp_array_encoder
 
 
 def execute(args):
     lst, _ = store.get(args[1], ([], None))
+    if len(args) > 2:
+        n = int(args[2])
+        popped, lst[:n] = lst[:n], []
+        return resp_array_encoder(popped)
     return resp_encoder(lst.pop(0) if lst else None)
