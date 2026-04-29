9cd70edd4574673d74e930ec3b05d3a9026d5bfd
Ronit Mishra <12792626+pyronlaboratory@users.noreply.github.com>
2026-04-29 18:16:25 +0530

    codecrafters submit [skip ci]

--- app/commands/xadd.py
+++ app/commands/xadd.py
@@ -5,6 +5,22 @@ ERR_SMALL = b"-ERR The ID specified in XADD is equal or smaller than the target 
 ERR_ZERO = b"-ERR The ID specified in XADD must be greater than 0-0\r\n"
 
 
+def resolve_id(entry_id, entries):
+    ms, seq = entry_id.split(b"-")
+    ms = int(ms)
+
+    if seq == b"*":
+        if entries:
+            last_ms, last_seq = parse_id(entries[-1][0])
+            seq = last_seq + 1 if last_ms == ms else 0
+        else:
+            seq = 1 if ms == 0 else 0
+    else:
+        seq = int(seq)
+
+    return ms, seq
+
+
 def parse_id(entry_id):
     ms, seq = entry_id.split(b"-")
     return int(ms), int(seq)
@@ -12,21 +28,18 @@ def parse_id(entry_id):
 
 def execute(args):
     key, entry_id = args[1], args[2]
-    ms, seq = parse_id(entry_id)
+    entries, _ = store.get(key, ([], None))
+    ms, seq = resolve_id(entry_id, entries)
 
     if (ms, seq) == (0, 0):
         return ERR_ZERO
+    if entries and (ms, seq) <= parse_id(entries[-1][0]):
+        return ERR_SMALL
 
-    entries, _ = store.get(key, ([], None))
-
-    if entries:
-        last_ms, last_seq = parse_id(entries[-1][0])
-        if (ms, seq) <= (last_ms, last_seq):
-            return ERR_SMALL
-
+    resolved_id = f"{ms}-{seq}".encode()
     if key not in store:
         store[key] = ([], None)
     store[key][0].append(
-        (entry_id, {args[i]: args[i + 1] for i in range(3, len(args), 2)})
+        (resolved_id, {args[i]: args[i + 1] for i in range(3, len(args), 2)})
     )
-    return resp_encoder(entry_id)
+    return resp_encoder(resolved_id)
